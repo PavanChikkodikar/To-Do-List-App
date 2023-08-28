74d9ea77067be6a68fcbed33c732fd412677a978
PavanChikkodikar <pavanchikkodikar@gmail.com>
2023-08-28 15:33:55 +0530

    Updating..

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -4,12 +4,25 @@ const errorMessage = document.getElementById("error-message");
 loginForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+
     errorMessage.textContent = "";
+
+    if (usernameInput.value.trim() === "") {
+        errorMessage.textContent = "Username is required";
+        return;
+    }
+
+    if (passwordInput.value.trim() === "") {
+        errorMessage.textContent = "Password is required";
+        return;
+    }
+
     const loginButton = document.querySelector("button[type='submit']");
     loginButton.disabled = true;
 
     setTimeout(function() {
         window.location.href = "/ToDoListApp/index.html";
-    }, 1000); 
-
+    }, 1000);
 });
